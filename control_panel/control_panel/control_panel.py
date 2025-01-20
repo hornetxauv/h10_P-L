9c47d3d2eab9b940fdfb364c1a05be21593b797b
hxbb2024 <hxbb2024@gmail.com>
2025-01-20 09:01:35 +0800

    clean up

--- control_panel/control_panel/control_panel.py
+++ control_panel/control_panel/control_panel.py
@@ -1,7 +1,6 @@
 import cv2
 import threading
-from typing import Optional, TypedDict, Union
-from operator import itemgetter
+from typing import Optional, Union
 
 class ControlPanelItem():
     value: Union[float, int]
@@ -10,35 +9,18 @@ class ControlPanelItem():
     multiplier: Optional[int]
     set_to_int: Optional[bool]
 
-    # def __init__(self, value, maximum, minimum, multiplier, set_to_int):
     def __init__(self, value, maximum=255, minimum=0, multiplier=1, set_to_int=False):
         self.value = value
         self.maximum = maximum
         self.minimum = minimum
         self.multiplier = multiplier
         self.set_to_int = set_to_int
-        # self.maximum = maximum or 255
-        # self.minimum = minimum or 0
-        # self.multiplier = multiplier or 1
-        # self.set_to_int = set_to_int or False
-
-# a=ControlPanelItem(value=1)
-# a.get('maximum')
 
 def create_control_panel(control_panel_name: str, controls, **kwargs):#: dict[str, ControlPanelItem]):
     cv2.namedWindow(control_panel_name)
     cv2.resizeWindow(control_panel_name, 700, 1500)
     for control in controls.items():
         name, v = control
-        # default_value = None
-        # value, maximum, minimum, multiplier, set_to_int = [*v, *([default_value] * (4 - len(v)))] # https://stackoverflow.com/a/59303329/7577786
-        # value, maximum, minimum, multiplier, set_to_int = itemgetter('value', 'maximum', 'minimum', 'multiplier', 'set_to_int')(v)
-        # value, maximum, minimum, multiplier, set_to_int = [v.get(f) for f in ['value', 'maximum', 'minimum', 'multiplier', 'set_to_int']]
-        # value, maximum, minimum, multiplier, set_to_int = v
-        # maximum = maximum if maximum else 255
-        # minimum = minimum if minimum != None else 0
-        # multiplier = multiplier if multiplier != None else 1
-        # set_to_int = set_to_int if set_to_int else False
 
         # Use default arguments to capture current 'name' value
         def on_change(val, name=name, minimum=v.minimum, multiplier=v.multiplier):
@@ -53,12 +35,8 @@ def create_control_panel(control_panel_name: str, controls, **kwargs):#: dict[st
             int(v.maximum*v.multiplier), 
             on_change
         )
-    print(kwargs)
     no_new_thread = kwargs.get("no_new_thread") or False
-    print(no_new_thread)
     if not no_new_thread:
         input_thread = threading.Thread(target=cv2.waitKey)
         input_thread.daemon = True
         input_thread.start()
-
-# create_control_panel('asd',{})
